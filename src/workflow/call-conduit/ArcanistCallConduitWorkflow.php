b6fb0f7a6b155f54947b6e3d56c691340ecc3f55
epriestley <git@epriestley.com>
2011-06-22 17:52:37 -0700

    Improve documentation for 'arc call-conduit'
    Summary:
    This documentation isn't terribly clear and it isn't obvious how to use the
    workflow. Make it more clear and provide examples.
    
    Test Plan:
    Ran "arc help call-conduit"
    
    Reviewed By: gc3
    Reviewers: gc3, aran, jungejason
    CC: aran, gc3, epriestley
    Differential Revision: 502

--- src/workflow/call-conduit/ArcanistCallConduitWorkflow.php
+++ src/workflow/call-conduit/ArcanistCallConduitWorkflow.php
@@ -27,10 +27,18 @@ class ArcanistCallConduitWorkflow extends ArcanistBaseWorkflow {
     return phutil_console_format(<<<EOTEXT
       **call-conduit** __method__
           Supports: http, https
-          Allows you to make a raw Conduit method call. Call parameters are read
-          as a JSON blob from stdin. Results are written to stdout as a JSON
-          blob. This workflow is primarily useful for writing scripts which
-          integrate with Phabricator.
+          Allows you to make a raw Conduit method call:
+
+            - Run this command from a working directory.
+            - Call parameters are REQUIRED and read as a JSON blob from stdin.
+            - Results are written to stdout as a JSON blob.
+
+          This workflow is primarily useful for writing scripts which integrate
+          with Phabricator. Examples:
+
+            $ echo '{}' | arc call-conduit conduit.ping
+            $ echo '{"phid":"PHID-FILE-xxxx"}' | arc call-conduit file.download
+
 EOTEXT
       );
   }
