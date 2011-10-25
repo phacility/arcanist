phacility/arcanist

commit d762311a9de9ae331949743e7b37fc920b7c6dff

arc lint: add support for PyLint
Summary:
Provides a lint class as a wrapper around the external project PyLint.
This exposes some arc config variables to control the behavior:

lint.pylint.prefix - non-standard installation location of pylint
lint.pylint.logilab_astng.prefix - non-standard installation location
  of logilab-astng, a dependency of pylint
lint.pylint.logilab_common.prefix - non-standard installation location
  of logilab-common, a dependency of pylint
lint.pylint.codes.{error,warning,advice} - regexes matching against
  PyLint message codes which should trigger arc errors/warnings/advice
lint.pylint.options - options to pass PyLint

Test Plan:
used to lint python code

Reviewed By: epriestley
Reviewers: epriestley, jungejason
CC: aran, epriestley
Differential Revision: 343

src/lint/linter/pylint/ArcanistPyLintLinter.php
@@ -1,0 +1,168 @@
+<?php
+
+/*
+ * Copyright 2011 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Uses "PyLint" to detect various errors in Python code.
+ *
+ * @group linter
+ */
+class ArcanistPyLintLinter extends ArcanistLinter {
+
+  private function getMessageCodeSeverity($code) {
+    // The config file defines how PyLint message codes translate to
+    // arcanist severities.  The config options provide regex's to
+    // match against the message codes generated by PyLint.  Severity's
+    // are matched in the order of errors, warnings, then advice.
+    // The first severity that matches, in that order, is returned.
+    $working_copy = $this->getEngine()->getWorkingCopy();
+    $code_map = array(
+      ArcanistLintSeverity::SEVERITY_ERROR =>
+          $working_copy->getConfig('lint.pylint.codes.error'),
+      ArcanistLintSeverity::SEVERITY_WARNING =>
+          $working_copy->getConfig('lint.pylint.codes.warning'),
+      ArcanistLintSeverity::SEVERITY_ADVICE =>
+          $working_copy->getConfig('lint.pylint.codes.advice'),
+    );
+
+    foreach ($code_map as $sev => $codes) {
+      if ($codes !== null) {
+        foreach ($codes as $code_re) {
+          if (preg_match("/{$code_re}/", $code)) {
+            return $sev;
+          }
+        }
+      }
+    }
+
+    // If the message code doesn't match any of the provided regex's,
+    // then just disable it.
+    return ArcanistLintSeverity::SEVERITY_DISABLED;
+  }
+
+  private function getPyLintPath() {
+    $pylint_bin = "pylint";
+
+    // Use the PyLint prefix specified in the config file
+    $working_copy = $this->getEngine()->getWorkingCopy();
+    $prefix = $working_copy->getConfig('lint.pylint.prefix');
+    if ($prefix !== null) {
+      $pylint_bin = $prefix."/bin/".$pylint_bin;
+    }
+
+    return $pylint_bin;
+  }
+
+  private function getPyLintPythonPath() {
+    // Get non-default install locations for pylint and its dependencies
+    // libraries.
+    $working_copy = $this->getEngine()->getWorkingCopy();
+    $prefixes = array(
+      $working_copy->getConfig('lint.pylint.prefix'),
+      $working_copy->getConfig('lint.pylint.logilab_astng.prefix'),
+      $working_copy->getConfig('lint.pylint.logilab_common.prefix'),
+    );
+
+    // Add the libraries to the python search path
+    $python_path = array();
+    foreach ($prefixes as $prefix) {
+      if ($prefix !== null) {
+        $python_path[] = $prefix.'/lib/python2.6/site-packages';
+      }
+    }
+
+    $python_path[] = '';
+    return implode(":", $python_path);
+  }
+
+  private function getPyLintOptions() {
+    // Options to pass the PyLint
+    // - '-rn': don't print lint report/summary at end
+    // - '-iy': show message codes for lint warnings/errors
+    $options = array('-rn',  '-iy');
+
+    // Add any options defined in the config file for PyLint
+    $working_copy = $this->getEngine()->getWorkingCopy();
+    $config_options = $working_copy->getConfig('lint.pylint.options');
+    if ($config_options !== null) {
+      $options += $config_options;
+    }
+
+    return implode(" ", $options);
+  }
+
+  public function willLintPaths(array $paths) {
+    return;
+  }
+
+  public function getLinterName() {
+    return 'PyLint';
+  }
+
+  public function getLintSeverityMap() {
+    return array();
+  }
+
+  public function getLintNameMap() {
+    return array();
+  }
+
+  public function lintPath($path) {
+    $pylint_bin = $this->getPyLintPath();
+    $python_path = $this->getPyLintPythonPath();
+    $options = $this->getPyLintOptions();
+    $path_on_disk = $this->getEngine()->getFilePathOnDisk($path);
+
+    try {
+      list($stdout, $_) = execx(
+          "/usr/bin/env PYTHONPATH=%s\$PYTHONPATH ".
+            "{$pylint_bin} {$options} {$path_on_disk}",
+          $python_path);
+    } catch (CommandException $e) {
+      // PyLint will return a non-zero exit code if warnings/errors are found.
+      // Therefore we detect command failure by checking that the stderr is
+      // some non-expected value.
+      if ($e->getStderr() !== "No config file found, ".
+                              "using default configuration\n") {
+        throw $e;
+      }
+
+      $stdout = $e->getStdout();
+    }
+
+    $lines = explode("\n", $stdout);
+    $messages = array();
+    foreach ($lines as $line) {
+      $matches = null;
+      if (!preg_match('/([A-Z]\d+): *(\d+): *(.*)$/', $line, $matches)) {
+        continue;
+      }
+      foreach ($matches as $key => $match) {
+        $matches[$key] = trim($match);
+      }
+      $message = new ArcanistLintMessage();
+      $message->setPath($path);
+      $message->setLine($matches[2]);
+      $message->setCode($matches[1]);
+      $message->setName($this->getLinterName()." ".$matches[1]);
+      $message->setDescription($matches[3]);
+      $message->setSeverity($this->getMessageCodeSeverity($matches[1]));
+      $this->addLintMessage($message);
+    }
+  }
+
+}

src/__phutil_library_map__.php
@@ -62,6 +62,7 @@ phutil_register_library_map(array(
     'ArcanistPhutilTestCase' => 'unit/engine/phutil/testcase',
     'ArcanistPhutilTestTerminatedException' => 'unit/engine/phutil/testcase/exception',
     'ArcanistPyFlakesLinter' => 'lint/linter/pyflakes',
+    'ArcanistPyLintLinter' => 'lint/linter/pylint',
     'ArcanistRepositoryAPI' => 'repository/api/base',
     'ArcanistShellCompleteWorkflow' => 'workflow/shell-complete',
     'ArcanistSubversionAPI' => 'repository/api/subversion',
@@ -115,6 +116,7 @@ phutil_register_library_map(array(
     'ArcanistPatchWorkflow' => 'ArcanistBaseWorkflow',
     'ArcanistPhutilModuleLinter' => 'ArcanistLinter',
     'ArcanistPyFlakesLinter' => 'ArcanistLinter',
+    'ArcanistPyLintLinter' => 'ArcanistLinter',
     'ArcanistShellCompleteWorkflow' => 'ArcanistBaseWorkflow',
     'ArcanistSubversionAPI' => 'ArcanistRepositoryAPI',
     'ArcanistSvnHookPreCommitWorkflow' => 'ArcanistBaseWorkflow',

src/lint/linter/pylint/__init__.php
@@ -1,0 +1,16 @@
+<?php
+/**
+ * This file is automatically generated. Lint this module to rebuild it.
+ * @generated
+ */
+
+
+
+phutil_require_module('arcanist', 'lint/linter/base');
+phutil_require_module('arcanist', 'lint/message');
+phutil_require_module('arcanist', 'lint/severity');
+
+phutil_require_module('phutil', 'future/exec');
+
+
+phutil_require_source('ArcanistPyLintLinter.php');

commit 00e5ba7ddce8b0a5f9df0cdb2df802d5f7f57698

fix bugs and documentation for ArcanistPyLintLinter
Summary:
- The top documentation is more readable, and the details about the severity
mapping regexps is moved to the top from inline -- they're important.
- array_merge() is used when appending command line args from
##lint.pylint.options## instead of '+'. The latter merges by key instead of
appending the two lists as intended.
- rely on the exit code instead of magic text that may or may not be there
depending on how ##pylint## is invoked.

These bugs were introduced in d762311a9de9ae331949743e7b37fc920b7c6dff.

Test Plan: ##arc lint --trace##'d a bunch of Python source files in a project,
with and without PyLint messages. Added some ##lint.pylint.options## to see
proper appending behavior.

Reviewers: epriestley

Reviewed By: epriestley

CC: aran, epriestley, cpiro

Differential Revision: 1045

src/lint/linter/pylint/ArcanistPyLintLinter.php
@@ -23,34 +23,41 @@
  *
  * You should be able to install pylint with ##sudo easy_install pylint##. If
  * your system is unusual, you can manually specify the location of pylint and
- * its dependencies by configuring these keys in your .arcrc:
+ * its dependencies by configuring these keys in your .arcconfig:
  *
  *   lint.pylint.prefix
  *   lint.pylint.logilab_astng.prefix
  *   lint.pylint.logilab_common.prefix
  *
- * You can specify additional command-line options to pass to PyLint by setting
- * this key:
+ * You can specify additional command-line options to pass to PyLint by
+ * setting ##lint.pylint.options##.
+
  *
- *   lint.pylint.options
- *
- * Now, configure which warnings and errors you want PyLint to raise by setting
- * these keys:
+ * Specify which PyLint messages map to which Arcanist messages by defining
+ * the following regular expressions:
  *
  *   lint.pylint.codes.error
  *   lint.pylint.codes.warning
  *   lint.pylint.codes.advice
  *
- * Set these to regular expressions -- for instance, if you want to raise all
- * PyLint errors as Arcanist errors, set this for ##lint.pylint.codes.error##:
+ * The regexps are run in that order; the first to match determines which
+ * Arcanist severity applies, if any. For example, to capture all PyLint
+ * "E...." errors as Arcanist errors, set ##lint.pylint.codes.error## to:
  *
  *    ^E.*
  *
- * You can also match more granular errors:
+ * You can also match more granularly:
  *
  *    ^E(0001|0002)$
  *
- * You can also provide a list of regular expressions.
+ * According to ##man pylint##, there are 5 kind of messages:
+ *
+ *   (C) convention, for programming standard violation
+ *   (R) refactor, for bad code smell
+ *   (W) warning, for python specific problems
+ *   (E) error, for probable bugs in the code
+ *   (F) fatal, if an error occurred which prevented pylint from
+ *       doing further processing.
  *
  * @group linter
  */
@@ -60,11 +67,6 @@ class ArcanistPyLintLinter extends ArcanistLinter {
 
     $working_copy = $this->getEngine()->getWorkingCopy();
 
-    // The config file defines how PyLint message codes translate to
-    // arcanist severities.  The config options provide regex's to
-    // match against the message codes generated by PyLint.  Severity's
-    // are matched in the order of errors, warnings, then advice.
-    // The first severity that matches, in that order, is returned.
     $error_regexp   = $working_copy->getConfig('lint.pylint.codes.error');
     $warning_regexp = $working_copy->getConfig('lint.pylint.codes.warning');
     $advice_regexp  = $working_copy->getConfig('lint.pylint.codes.advice');
@@ -150,16 +152,15 @@ class ArcanistPyLintLinter extends ArcanistLinter {
   }
 
   private function getPyLintOptions() {
-    // Options to pass the PyLint
-    // - '-rn': don't print lint report/summary at end
-    // - '-iy': show message codes for lint warnings/errors
+    // '-rn': don't print lint report/summary at end
+    // '-iy': show message codes for lint warnings/errors
     $options = array('-rn',  '-iy');
 
     // Add any options defined in the config file for PyLint
     $working_copy = $this->getEngine()->getWorkingCopy();
     $config_options = $working_copy->getConfig('lint.pylint.options');
     if ($config_options !== null) {
-      $options += $config_options;
+      $options = array_merge($options, $config_options);
     }
 
     return implode(" ", $options);
@@ -193,15 +194,15 @@ class ArcanistPyLintLinter extends ArcanistLinter {
             "{$pylint_bin} {$options} {$path_on_disk}",
           $python_path);
     } catch (CommandException $e) {
-      // PyLint will return a non-zero exit code if warnings/errors are found.
-      // Therefore we detect command failure by checking that the stderr is
-      // some non-expected value.
-      if ($e->getStderr() !== "No config file found, ".
-                              "using default configuration\n") {
+      if ($e->getError() == 32) {
+        // According to ##man pylint## the exit status of 32 means there was a
+        // usage error. That's bad, so actually exit abnormally.
         throw $e;
+      } else {
+        // The other non-zero exit codes mean there were messages issued,
+        // which is expected, so don't exit.
+        $stdout = $e->getStdout();
       }
-
-      $stdout = $e->getStdout();
     }
 
     $lines = explode("\n", $stdout);