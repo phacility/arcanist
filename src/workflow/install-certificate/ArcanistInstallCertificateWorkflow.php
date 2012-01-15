62662064871b1738a50e28626c01f9beb820655e
Marke Hallowell <indiefan@gmail.com>
2012-01-14 19:55:20 -0800

    Fix for `arc install-certificate` timeout.
    Summary:
    Extended the install-certificate workflow's timeout from 5
    seconds to rely on the ConduitClient default (30 seconds), which matches the HTTP interface.
    
    Test Plan: Run arc install-certificate
    
    Reviewers: epriestley
    
    CC: epriestley, aran
    
    Differential Revision: https://secure.phabricator.com/D1404

--- src/workflow/install-certificate/ArcanistInstallCertificateWorkflow.php
+++ src/workflow/install-certificate/ArcanistInstallCertificateWorkflow.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,7 +65,6 @@ EOTEXT
 
     echo "Trying to connect to server...\n";
     $conduit = new ConduitClient($uri);
-    $conduit->setTimeout(5);
     try {
       $conduit->callMethodSynchronous('conduit.ping', array());
     } catch (Exception $ex) {
