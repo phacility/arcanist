a3e0c26ea8e3bf5b6db968e8a0fe1fe637e9bf6e
vrana <jakubv@fb.com>
2013-02-08 14:30:25 -0800

    Delete newLintAtLine()
    Summary: Nobody needs it because `raiseLintAtLine()` returns the message.
    
    Test Plan:
      $ arc unit
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, Korvin
    
    Differential Revision: https://secure.phabricator.com/D4870

--- src/lint/linter/ArcanistLinter.php
+++ src/lint/linter/ArcanistLinter.php
@@ -151,17 +151,6 @@ abstract class ArcanistLinter {
     return $this->messages;
   }
 
-  protected function newLintAtLine($line, $char, $code, $desc) {
-    return id(new ArcanistLintMessage())
-      ->setPath($this->getActivePath())
-      ->setLine($line)
-      ->setChar($char)
-      ->setCode($this->getLintMessageFullCode($code))
-      ->setSeverity($this->getLintMessageSeverity($code))
-      ->setName($this->getLintMessageName($code))
-      ->setDescription($desc);
-  }
-
   protected function raiseLintAtLine(
     $line,
     $char,
@@ -170,7 +159,14 @@ abstract class ArcanistLinter {
     $original = null,
     $replacement = null) {
 
-    $message = $this->newLintAtLine($line, $char, $code, $desc)
+    $message = id(new ArcanistLintMessage())
+      ->setPath($this->getActivePath())
+      ->setLine($line)
+      ->setChar($char)
+      ->setCode($this->getLintMessageFullCode($code))
+      ->setSeverity($this->getLintMessageSeverity($code))
+      ->setName($this->getLintMessageName($code))
+      ->setDescription($desc)
       ->setOriginalText($original)
       ->setReplacementText($replacement);
 
