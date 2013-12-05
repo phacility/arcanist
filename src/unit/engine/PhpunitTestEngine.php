4c4fef5330a7800364473df4df924dc5787e1666
Lajos Veres <vlajos@gmail.com>
2013-12-05 09:55:38 +0000

    skip removed files when unit testing

--- src/unit/engine/PhpunitTestEngine.php
+++ src/unit/engine/PhpunitTestEngine.php
@@ -38,6 +38,11 @@ final class PhpunitTestEngine extends ArcanistBaseUnitTestEngine {
         continue;
       }
 
+      // We skip non-existing files. Probably they has been deleted.
+      if(!file_exists($path)){
+        continue;
+      }
+
       if (substr($path, -8) == 'Test.php') {
         // Looks like a valid test file name.
         $this->affectedTests[$path] = $path;
