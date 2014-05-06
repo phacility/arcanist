8523b98f396d4a2dbeb1dd58f2b1a071ad0f748e
Joshua Spence <josh@joshuaspence.com>
2014-05-05 20:42:40 -0700

    Add a `puppet-lint` linter.
    Summary: This linter is a wrapper around [[http://puppet-lint.com/ | puppet-lint[]].
    
    Test Plan: Wrote an executed unit tests.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D8990

---
+++ src/lint/linter/ArcanistPuppetLintLinter.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * A linter for Puppet files.
+ */
+final class ArcanistPuppetLintLinter extends ArcanistExternalLinter {
+
+  public function getLinterName() {
+    return 'PUPPETLINT';
+  }
+
+  public function getLinterConfigurationName() {
+    return 'puppet-lint';
+  }
+
+  public function getDefaultBinary() {
+    return 'puppet-lint';
+  }
+
+  public function getVersion() {
+    list($stdout) = execx('%C --version', $this->getExecutableCommand());
+
+    $matches = array();
+    $regex = '/^Puppet-lint (?P<version>\d+\.\d+\.\d+)$/';
+    if (preg_match($regex, $stdout, $matches)) {
+      return $matches['version'];
+    } else {
+      return false;
+    }
+  }
+
+  public function getInstallInstructions() {
+    return pht('Install puppet-lint using `gem install puppet-lint`.');
+  }
+
+  public function shouldExpectCommandErrors() {
+    return true;
+  }
+
+  public function supportsReadDataFromStdin() {
+    return false;
+  }
+
+  protected function getMandatoryFlags() {
+    return array(sprintf('--log-format=%s', implode('|', array(
+      '%{linenumber}',
+      '%{column}',
+      '%{kind}',
+      '%{check}',
+      '%{message}'))));
+  }
+
+  protected function parseLinterOutput($path, $err, $stdout, $stderr) {
+    $lines = phutil_split_lines($stdout, false);
+
+    $messages = array();
+    foreach ($lines as $line) {
+      $matches = explode('|', $line, 5);
+
+      if (count($matches) === 5) {
+        $message = new ArcanistLintMessage();
+        $message->setPath($path);
+        $message->setLine($matches[0]);
+        $message->setChar($matches[1]);
+        $message->setName(ucwords(str_replace('_', ' ', $matches[3])));
+        $message->setDescription(ucfirst($matches[4]));
+
+        switch ($matches[2]) {
+          case 'warning':
+            $message->setSeverity(ArcanistLintSeverity::SEVERITY_WARNING);
+            break;
+
+          case 'error':
+            $message->setSeverity(ArcanistLintSeverity::SEVERITY_ERROR);
+            break;
+
+          default:
+            $message->setSeverity(ArcanistLintSeverity::SEVERITY_ADVICE);
+            break;
+        }
+
+        $messages[] = $message;
+      }
+    }
+
+    if ($err && !$messages) {
+      return false;
+    }
+
+    return $messages;
+  }
+}
