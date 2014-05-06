ab5c1562c0f6a8736f687a1628f85ec25c13426b
Joshua Spence <josh@joshuaspence.com>
2014-05-05 20:15:35 -0700

    Add a JSON linter.
    Summary: Provide bindings for [[https://github.com/zaach/jsonlint | JSONLint]], which is a useful tool for linting and validating JSON. Theoretically, this could be done with pure PHP, however it would not be trivial (`json_decode`, for example, does not provide any context as to JSON validation errors).
    
    Test Plan: Wrote and executed unit tests.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D8988

---
+++ src/lint/linter/ArcanistJSONLintLinter.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * A linter for JSON files.
+ */
+final class ArcanistJSONLintLinter extends ArcanistExternalLinter {
+  public function getLinterName() {
+    return 'JSON';
+  }
+
+  public function getLinterConfigurationName() {
+    return 'jsonlint';
+  }
+
+  public function getDefaultBinary() {
+    return 'jsonlint';
+  }
+
+  public function getVersion() {
+    // NOTE: `jsonlint --version` returns a non-zero exit status.
+    list($err, $stdout) = exec_manual(
+      '%C --version',
+      $this->getExecutableCommand());
+
+    $matches = array();
+    if (preg_match('/^(?P<version>\d+\.\d+\.\d+)$/', $stdout, $matches)) {
+      $version = $matches['version'];
+    } else {
+      return false;
+    }
+  }
+
+  public function getInstallInstructions() {
+    return pht('Install jsonlint using `npm install -g jsonlint`.');
+  }
+
+  public function shouldExpectCommandErrors() {
+    return true;
+  }
+
+  public function supportsReadDataFromStdin() {
+    return true;
+  }
+
+  protected function getMandatoryFlags() {
+    return array(
+      '--compact',
+    );
+  }
+
+  protected function parseLinterOutput($path, $err, $stdout, $stderr) {
+    $lines = phutil_split_lines($stderr, false);
+
+    $messages = array();
+    foreach ($lines as $line) {
+      $matches = null;
+      $match = preg_match(
+        '/^(?:(?<path>.+): )?' .
+        'line (?<line>\d+), col (?<column>\d+), ' .
+        '(?<description>.*)$/',
+        $line,
+        $matches);
+
+      if ($match) {
+        $message = new ArcanistLintMessage();
+        $message->setPath($path);
+        $message->setLine($matches['line']);
+        $message->setChar($matches['column']);
+        $message->setCode($this->getLinterName());
+        $message->setDescription(ucfirst($matches['description']));
+        $message->setSeverity(ArcanistLintSeverity::SEVERITY_ERROR);
+
+        $messages[] = $message;
+      }
+    }
+
+    if ($err && !$messages) {
+      return false;
+    }
+
+    return $messages;
+  }
+}
