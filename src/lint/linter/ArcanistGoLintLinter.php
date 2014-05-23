042aa2ee38df81bdae7002f35b5b723a88b66f8b
Joshua Spence <josh@joshuaspence.com>
2014-05-23 07:59:57 -0700

    Add an `ArcanistGoLintLinter`.
    Summary: Create bindings for `golint` as an external linter.
    
    Test Plan: Wrote and executed unit tests.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D9246

---
+++ src/lint/linter/ArcanistGoLintLinter.php
@@ -0,0 +1,60 @@
+<?php
+
+final class ArcanistGoLintLinter extends ArcanistExternalLinter {
+
+  public function getInfoName() {
+    return 'Golint';
+  }
+
+  public function getInfoURI() {
+    return 'https://github.com/golang/lint';
+  }
+
+  public function getInfoDescription() {
+    return pht('Golint is a linter for Go source code.');
+  }
+
+  public function getLinterName() {
+    return 'GOLINT';
+  }
+
+  public function getLinterConfigurationName() {
+    return 'golint';
+  }
+
+  public function getDefaultBinary() {
+    return 'golint';
+  }
+
+  public function getInstallInstructions() {
+    return pht('Install Golint using `go get github.com/golang/lint/golint`.');
+  }
+
+  protected function canCustomizeLintSeverities() {
+    return true;
+  }
+
+  protected function parseLinterOutput($path, $err, $stdout, $stderr) {
+    $lines = phutil_split_lines($stdout, false);
+
+    $messages = array();
+    foreach ($lines as $line) {
+      $matches = explode(':', $line, 4);
+
+      if (count($matches) === 4) {
+        $message = new ArcanistLintMessage();
+        $message->setPath($path);
+        $message->setLine($matches[1]);
+        $message->setChar($matches[2]);
+        $message->setCode($this->getLinterName());
+        $message->setDescription(ucfirst(trim($matches[3])));
+        $message->setSeverity(ArcanistLintSeverity::SEVERITY_ADVICE);
+
+        $messages[] = $message;
+      }
+    }
+
+    return $messages;
+  }
+
+}
