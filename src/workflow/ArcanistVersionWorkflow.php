b7bb6c834830e5ea413323284e1b71663fdaa19e
Joshua Spence <josh@joshuaspence.com>
2014-05-23 19:35:33 -0700

    Add a `version` workflow to arcanist.
    Summary: A `version` workflow would be useful, especially for less technical users. Additionally, whenever I am faced with a new command I reasonably expect `$CMD [--help|help]` and `$CMD [--version|version]` to work.
    
    Test Plan: Ran `arc version`.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D9265

---
+++ src/workflow/ArcanistVersionWorkflow.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Display the current version of Arcanist.
+ */
+final class ArcanistVersionWorkflow extends ArcanistBaseWorkflow {
+
+  public function getWorkflowName() {
+    return 'version';
+  }
+
+  public function getCommandSynopses() {
+    return phutil_console_format(<<<EOTEXT
+      **version** [__options__]
+EOTEXT
+      );
+  }
+
+  public function getCommandHelp() {
+    return phutil_console_format(pht(<<<EOTEXT
+          Supports: cli
+          Shows the current version of arcanist.
+EOTEXT
+      ));
+  }
+
+  public function run() {
+    $console = PhutilConsole::getConsole();
+
+    if (!Filesystem::binaryExists('git')) {
+      throw new ArcanistUsageException(
+        'Cannot display current version without having `git` installed.');
+    }
+
+    $roots = array(
+      'arcanist' => dirname(phutil_get_library_root('arcanist')),
+      'libphutil' => dirname(phutil_get_library_root('phutil')),
+    );
+
+    foreach ($roots as $lib => $root) {
+      $working_copy = ArcanistWorkingCopyIdentity::newFromPath($root);
+      $configuration_manager = clone $this->getConfigurationManager();
+      $configuration_manager->setWorkingCopyIdentity($working_copy);
+      $repository = ArcanistRepositoryAPI::newAPIFromConfigurationManager(
+        $configuration_manager);
+
+      if (!Filesystem::pathExists($repository->getMetadataPath())) {
+        throw new ArcanistUsageException("{$lib} is not a git working copy.");
+      }
+
+      list($stdout) = $repository->execxLocal('log -1 --format=%s', '%H %ct');
+      list($commit, $timestamp) = explode(' ', $stdout);
+
+      $console->writeOut("%s %s (%s)\n",
+        $lib,
+        $commit,
+        date('j M Y', (int)$timestamp));
+    }
+  }
+
+}
