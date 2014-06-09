phacility/arcanist

commit e0ee3e8a07ba5fdfd5d1f5aa76e475cc6f1cd383

Capture maximum version information in `php_compat_info`.
Summary: Ref T5141. In order to be able to warn when deprecated functions are used, we need to be aware of from which version the functions were deprecated.

Test Plan: Ran `arc lint` and made sure no unexpected warnings were raised.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: epriestley, Korvin

Maniphest Tasks: T5141

Differential Revision: https://secure.phabricator.com/D9248

src/lint/linter/ArcanistXHPASTLinter.php
@@ -456,7 +456,7 @@ final class ArcanistXHPASTLinter extends ArcanistBaseXHPASTLinter {
       $name = strtolower($node->getConcreteString());
       $version = idx($compat_info['functions'], $name);
       $windows = idx($compat_info['functions_windows'], $name);
-      if ($version && version_compare($version, $required, '>')) {
+      if ($version && version_compare($version['min'], $required, '>')) {
         $this->raiseLintAtNode(
           $node,
           self::LINT_PHP_53_FEATURES,
@@ -489,7 +489,7 @@ final class ArcanistXHPASTLinter extends ArcanistBaseXHPASTLinter {
       $name = strtolower($node->getConcreteString());
       $version = idx($compat_info['interfaces'], $name);
       $version = idx($compat_info['classes'], $name, $version);
-      if ($version && version_compare($version, $required, '>')) {
+      if ($version && version_compare($version['min'], $required, '>')) {
         $this->raiseLintAtNode(
           $node,
           self::LINT_PHP_53_FEATURES,

scripts/update_compat_info.php
@@ -25,7 +25,11 @@ foreach (array('functions', 'classes', 'interfaces') as $type) {
     $name = strtolower($name);
     $versions = reset($versions);
     list($min, $max) = $versions;
-    $output[$type][$name] = $min;
+    $output[$type][$name] = array(
+      'min' => nonempty($min, null),
+      'max' => nonempty($max, null),
+    );
+
     if ($type == 'functions' && isset($versions[4])) {
       $params = explode(', ', $versions[4]);
       foreach ($params as $i => $version) {

commit 86eec5b44c71e256eb171bff6ab34c854cddb8b4

Fix "array to string" conversion error.
Summary: Fixes T5298. This bug was probably introduced in D9248. It looks like I forgot to update some references to `$version`.

Test Plan: Ran `arc lint --everything` in rPHU.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: epriestley, Korvin

Maniphest Tasks: T5298

Differential Revision: https://secure.phabricator.com/D9434

src/lint/linter/ArcanistXHPASTLinter.php
@@ -462,7 +462,7 @@ final class ArcanistXHPASTLinter extends ArcanistBaseXHPASTLinter {
           $node,
           self::LINT_PHP_53_FEATURES,
           "This codebase targets PHP 5.2.3, but `{$name}()` was not ".
-          "introduced until PHP {$version}.");
+          "introduced until PHP {$version['min']}.");
       } else if (array_key_exists($name, $compat_info['params'])) {
         $params = $call->getChildOfType(1, 'n_CALL_PARAMETER_LIST');
         foreach (array_values($params->getChildren()) as $i => $param) {
@@ -495,7 +495,7 @@ final class ArcanistXHPASTLinter extends ArcanistBaseXHPASTLinter {
           $node,
           self::LINT_PHP_53_FEATURES,
           "This codebase targets PHP 5.2.3, but `{$name}` was not ".
-          "introduced until PHP {$version}.");
+          "introduced until PHP {$version['min']}.");
       }
     }
 