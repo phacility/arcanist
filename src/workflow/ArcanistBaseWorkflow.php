08bad11bc85a82d17455c49457258cbbc7bd2c2b
Joshua Spence <josh@joshuaspence.com>
2014-06-11 05:51:06 -0700

    Remove the `ArcanistBaseWorkflow::getUserGUID` function.
    Summary: This function has been deprecated for a long time (see rARC8150fdf044818c503a588c1b3ef0ddbb93cfa1be). It should be safe to remove it now.
    
    Test Plan: N/A
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D9465

--- src/workflow/ArcanistBaseWorkflow.php
+++ src/workflow/ArcanistBaseWorkflow.php
@@ -450,18 +450,6 @@ abstract class ArcanistBaseWorkflow extends Phobject {
   }
 
   /**
-   * Deprecated. See @{method:getUserPHID}.
-   *
-   * @deprecated
-   */
-  final public function getUserGUID() {
-    phutil_deprecated(
-      'ArcanistBaseWorkflow::getUserGUID',
-      'This method has been renamed to getUserPHID().');
-    return $this->getUserPHID();
-  }
-
-  /**
    * Return the username for the user once they've authenticated via Conduit.
    *
    * @return string Authenticated username.
