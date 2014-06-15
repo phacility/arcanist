523d767646d2a87ef187982ef8be6d1bf5daa8ae
Joshua Spence <josh@joshuaspence.com>
2014-06-16 05:42:43 +1000

    Add a unit test to check `src/__phutil_library_map__.php`.
    Summary: See D9557. Add a unit test to ensure that `src/__phutil_library_map__.php` is up-to-date.
    
    Test Plan: Ran `arc unit`.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D9558

---
+++ src/__tests__/ArcanistInfrastructureTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+final class ArcanistInfrastructureTestCase extends ArcanistTestCase {
+  /**
+   * This is more of an acceptance test case instead of a unit test. It verifies
+   * that all symbols can be loaded correctly. It can catch problems like
+   * missing methods in descendants of abstract base classes.
+   */
+  public function testEverythingImplemented() {
+    id(new PhutilSymbolLoader())->selectAndLoadSymbols();
+    $this->assertTrue(true);
+  }
+}
