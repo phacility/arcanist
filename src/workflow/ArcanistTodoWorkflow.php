06fe0f1ef995e8266adaba2a4f7de29a593ef4e9
Zheng Shao <zshao@dropbox.com>
2014-07-17 21:28:56 -0700

    arc todo --priority

--- src/workflow/ArcanistTodoWorkflow.php
+++ src/workflow/ArcanistTodoWorkflow.php
@@ -50,6 +50,12 @@ EOTEXT
         'repeat' => false,
         'help'   => pht('Owner of the new task.'),
       ),
+      'priority' => array(
+        'param'  => 'priority',
+        'short'  => 'P',
+        'repeat' => false,
+        'help'   => pht('Priority value (99, 90, 85, 80, 50, 25, 0)'),
+      ),
       'project' => array(
         'param'  => 'project',
         'repeat' => true,
@@ -62,6 +68,7 @@ EOTEXT
     $summary = implode(' ', $this->getArgument('summary'));
     $ccs = $this->getArgument('cc');
     $owner = $this->getArgument('owner');
+    $priority = $this->getArgument('priority');
     $slugs = $this->getArgument('project');
 
     $conduit = $this->getConduit();
@@ -100,6 +107,10 @@ EOTEXT
         $phids[] = $info['phid'];
       }
       $args['ownerPHID'] = $phids[0];
+    }
+
+    if ($priority !== null) {
+      $args['priority'] = $priority;
     }
 
     if ($slugs) {
