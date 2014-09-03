33e6e26f5e3a2777ba35b8275272b546c16b9dbf
Joshua Newman <josh@coursera.org>
2014-09-02 23:33:52 -0700

    Add Scalastyle linter that ignores output.
    Parse Scalastyle lint output.
    
    Add a Scalastyle linter
    
    Summary:
    I've been using Phabricator a lot recently for Scala projects. We thought it might be useful to add linting to our code review process, and I didn't see any current Scala linters in Arcanist, so I put together a Scalastyle linter. I didn't find existing revisions or tasks related to Scala linter configuration, but if there are I'm happy to wait or contribute to that instead of pushing this change.
    
    I'm not too familiar with Arcanist contribution processes; please let me know if I'm doing anything very wrongly!
    
    Test Plan:
    I ran this linter locally.
    
    I tried adding a unit test but I wasn't sure how to configure the paths to the Scalastyle JAR and configuration file in the test environment. Are there examples of other tests that do this?
    
    Reviewers: #blessed_reviewers, epriestley
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D10405
    
    Add check for empty stdout.

---
+++ src/lint/linter/ArcanistScalastyleLinter.php
@@ -0,0 +1,178 @@
+<?php
+
+final class ArcanistScalastyleLinter extends ArcanistExternalLinter {
+
+  private $jarPath = null;
+  private $configPath = null;
+
+  public function getInfoURI() {
+    return 'http://www.scalastyle.org/';
+  }
+
+  public function getInfoDescription() {
+    return 'Scalastyle linter for Scala code';
+  }
+
+  public function getLinterName() {
+    return 'scalastyle';
+  }
+
+  public function getLinterConfigurationName() {
+    return 'scalastyle';
+  }
+
+  public function getDefaultBinary() {
+    return 'java';
+  }
+
+  public function getInstallInstructions() {
+    return 'See http://www.scalastyle.org/command-line.html';
+  }
+
+  public function shouldExpectCommandErrors() {
+    return true;
+  }
+
+  protected function getMandatoryFlags() {
+    if ($this->jarPath === null) {
+      throw new ArcanistUsageException(
+        pht('Scalastyle JAR path must be configured.'));
+    }
+    if ($this->configPath === null) {
+      throw new ArcanistUsageException(
+        pht('Scalastyle config XML path must be configured.'));
+    }
+
+    return array(
+      '-jar', $this->jarPath,
+      '--config', $this->configPath,
+      '--quiet', 'true');
+  }
+
+  protected function getDefaultFlags() {
+    return array();
+  }
+
+  protected function parseLinterOutput($path, $err, $stdout, $stderr) {
+
+    $messages = array();
+
+    $output = trim($stdout);
+    if (strlen($output) === 0) {
+      return $messages;
+    }
+
+    $lines = explode(PHP_EOL, $output);
+
+    foreach ($lines as $line) {
+      $lintMessage = id(new ArcanistLintMessage())
+        ->setPath($path)
+        ->setCode($this->getLinterName());
+
+      $matches = array();
+      if (preg_match('/^([a-z]+)/', $line, $matches)) {
+        switch ($matches[1]) {
+          case 'warning':
+            $lintMessage->setSeverity(ArcanistLintSeverity::SEVERITY_WARNING);
+            break;
+          case 'error':
+            $lintMessage->setSeverity(ArcanistLintSeverity::SEVERITY_ERROR);
+            break;
+        }
+      }
+
+      $matches = array();
+      if (preg_match('/message=([^=]+ )/', $line, $matches)) {
+        $lintMessage->setDescription(trim($matches[1]));
+      } else if (preg_match('/message=([^=]+$)/', $line, $matches)) {
+        $lintMessage->setDescription(trim($matches[1]));
+      }
+
+      $matches = array();
+      if (preg_match('/line=([^=]+ )/', $line, $matches)) {
+        $lintMessage->setLine(trim($matches[1]));
+      } else if (preg_match('/line=([^=]+$)/', $line, $matches)) {
+        $lintMessage->setLine(trim($matches[1]));
+      }
+
+      $matches = array();
+      if (preg_match('/column=([^=]+ )/', $line, $matches)) {
+        $lintMessage->setChar(trim($matches[1]));
+      } else if (preg_match('/column=([^=]+$)/', $line, $matches)) {
+        $lintMessage->setChar(trim($matches[1]));
+      }
+
+      $messages[] = $lintMessage;
+    }
+
+    return $messages;
+  }
+
+  public function getLinterConfigurationOptions() {
+    $options = array(
+      'jar' => array(
+        'type' => 'optional string | list<string>',
+        'help' => pht(
+          'Specify a string (or list of strings) identifying the Scalastyle '.
+          'JAR file.')
+      ),
+      'config' => array(
+        'type' => 'optional string | list<string>',
+        'help' => pht(
+          'Specify a string (or list of strings) identifying the Scalastyle '.
+          'config XML file.')
+      ),
+    );
+
+    return $options + parent::getLinterConfigurationOptions();
+  }
+
+  public function setLinterConfigurationValue($key, $value) {
+    switch ($key) {
+      case 'jar':
+        $working_copy = $this->getEngine()->getWorkingCopy();
+        $root = $working_copy->getProjectRoot();
+
+        foreach ((array)$value as $path) {
+          if (Filesystem::pathExists($path)) {
+            $this->jarPath = $path;
+            return;
+          }
+
+          $path = Filesystem::resolvePath($path, $root);
+
+          if (Filesystem::pathExists($path)) {
+            $this->jarPath = $path;
+            return;
+          }
+        }
+
+        throw new ArcanistUsageException(
+          pht('None of the configured Scalastyle JARs can be located.'));
+
+      case 'config':
+        $working_copy = $this->getEngine()->getWorkingCopy();
+        $root = $working_copy->getProjectRoot();
+
+        foreach ((array)$value as $path) {
+          if (Filesystem::pathExists($path)) {
+            $this->configPath = $path;
+            return;
+          }
+
+          $path = Filesystem::resolvePath($path, $root);
+
+          if (Filesystem::pathExists($path)) {
+            $this->configPath = $path;
+            return;
+          }
+        }
+
+        throw new ArcanistUsageException(
+          pht('None of the configured Scalastyle configs can be located.'));
+    }
+
+    return parent::setLinterConfigurationValue($key, $value);
+  }
+
+}
