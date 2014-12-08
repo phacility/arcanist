4ca3463df684795b0cec6798256beebb845ee572
Joshua Spence <josh@joshuaspence.com>
2014-12-08 23:31:21 +1100

    Extend `LINT_BRACE_FORMATTING` to support more scenarios
    Summary: Extend the `ArcanistXHPASTLinter::LINT_BRACE_FORMATTING` rule to support a bunch of extra cases.
    
    Test Plan: Added test cases.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D10572

--- src/lint/linter/ArcanistXHPASTLinter.php
+++ src/lint/linter/ArcanistXHPASTLinter.php
@@ -902,6 +902,39 @@ final class ArcanistXHPASTLinter extends ArcanistBaseXHPASTLinter {
             'Use braces to surround a statement block.');
       }
     }
+
+    $nodes = $root->selectDescendantsOfTypes(array(
+      'n_DO_WHILE',
+      'n_ELSE',
+      'n_ELSEIF',
+    ));
+    foreach ($nodes as $list) {
+      $tokens = $list->getTokens();
+      if (!$tokens || last($tokens)->getValue() != '}') {
+        continue;
+      }
+      list($before, $after) = $list->getSurroundingNonsemanticTokens();
+      if (!$before) {
+        $first = last($tokens);
+
+        $this->raiseLintAtToken(
+          $first,
+          self::LINT_BRACE_FORMATTING,
+          'Put opening braces on the same line as control statements and '.
+          'declarations, with a single space before them.',
+          ' '.$first->getValue());
+      } else if (count($before) === 1) {
+        $before = reset($before);
+        if ($before->getValue() !== ' ') {
+          $this->raiseLintAtToken(
+            $before,
+            self::LINT_BRACE_FORMATTING,
+            'Put opening braces on the same line as control statements and '.
+            'declarations, with a single space before them.',
+            ' ');
+        }
+      }
+    }
   }
 
   private function lintTautologicalExpressions(XHPASTNode $root) {
