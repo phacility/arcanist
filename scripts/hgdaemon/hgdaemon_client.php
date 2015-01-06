7e2df9a5bfc635b77c6412ef5a6569edd0e4e138
Joshua Spence <josh@joshuaspence.com>
2015-01-06 23:14:04 +1100

    `pht`ize some strings
    Summary: Self-explanatory.
    
    Test Plan: Eye-balled it.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D10596

--- scripts/hgdaemon/hgdaemon_client.php
+++ scripts/hgdaemon/hgdaemon_client.php
@@ -9,10 +9,10 @@ $args->parse(
   array(
     array(
       'name'      => 'skip-hello',
-      'help'      => 'Do not expect "capability" message when connecting. '.
-                     'The server must be configured not to send the message. '.
-                     'This deviates from the Mercurial protocol, but slightly '.
-                     'improves performance.',
+      'help'      => pht(
+        'Do not expect "capability" message when connecting. The server '.
+        'must be configured not to send the message. This deviates from '.
+        'the Mercurial protocol, but slightly improves performance.'),
     ),
     array(
       'name'      => 'repository',
@@ -22,7 +22,7 @@ $args->parse(
 
 $repo = $args->getArg('repository');
 if (count($repo) !== 1) {
-  throw new Exception('Specify exactly one working copy!');
+  throw new Exception(pht('Specify exactly one working copy!'));
 }
 $repo = head($repo);
 
@@ -34,7 +34,12 @@ $t_start = microtime(true);
 $result = $client->executeCommand(
   array('log', '--template', '{node}', '--rev', 2));
 
-$t_end   = microtime(true);
+$t_end = microtime(true);
 var_dump($result);
 
-echo "\nExecuted in ".((int)(1000000 * ($t_end - $t_start)))."us.\n";
+$console = PhutilConsole::getConsole();
+$console->writeOut(
+  "\n%s\n",
+  pht(
+    'Executed in %d us.',
+    ((int)(1000000 * ($t_end - $t_start)))));
