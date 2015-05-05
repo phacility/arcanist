0846c6aff5c4da7b0c3a9f959bf77368329c5775
Joshua Spence <josh@joshuaspence.com>
2015-05-05 20:56:45 +1000

    Remove commit linter
    Summary: Ref T7674. This linter doesn't make sense without commit hooks.
    
    Test Plan: N/A
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T7674
    
    Differential Revision: https://secure.phabricator.com/D12697

--- src/lint/linter/__tests__/ArcanistCommitLinterTestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-final class ArcanistCommitLinterTestCase extends ArcanistLinterTestCase {
-
-  public function testLinter() {
-    return $this->executeTestsInDirectory(dirname(__FILE__).'/commit/');
-  }
-
-}
