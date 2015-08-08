9e65fc65164a3fc82e6027f77b111fd8e9362afc
Joshua Spence <josh@joshuaspence.com>
2015-08-08 23:30:24 +1000

    Improve declaration formatting linter rule for anonymous functions
    Summary: Ref T8742. Anonymous functions should have a space after the `function` keyword. Additionally, ensure that there is a space before and after the `use` token.
    
    Test Plan: Modified existing test cases.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin
    
    Maniphest Tasks: T8742
    
    Differential Revision: https://secure.phabricator.com/D13804

--- src/lint/linter/xhpast/rules/ArcanistDeclarationParenthesesXHPASTLinterRule.php
+++ src/lint/linter/xhpast/rules/ArcanistDeclarationParenthesesXHPASTLinterRule.php
@@ -24,7 +24,7 @@ final class ArcanistDeclarationParenthesesXHPASTLinterRule
       $tokens = $params->getTokens();
 
       $first = head($tokens);
-      $last = last($tokens);
+      $last  = last($tokens);
 
       $leading = $first->getNonsemanticTokensBefore();
       $leading_text = implode('', mpull($leading, 'getValue'));
@@ -32,14 +32,27 @@ final class ArcanistDeclarationParenthesesXHPASTLinterRule
       $trailing = $last->getNonsemanticTokensBefore();
       $trailing_text = implode('', mpull($trailing, 'getValue'));
 
-      if (preg_match('/^\s+$/', $leading_text)) {
-        $this->raiseLintAtOffset(
-          $first->getOffset() - strlen($leading_text),
-          pht(
-            'Convention: no spaces before opening parenthesis in '.
-            'function and method declarations.'),
-          $leading_text,
-          '');
+      if ($dec->getChildByIndex(2)->getTypeName() == 'n_EMPTY') {
+        // Anonymous functions.
+        if ($leading_text != ' ') {
+          $this->raiseLintAtOffset(
+            $first->getOffset() - strlen($leading_text),
+            pht(
+              'Convention: space before opening parenthesis in '.
+              'anonymous function declarations.'),
+            $leading_text,
+            ' ');
+        }
+      } else {
+        if (preg_match('/^\s+$/', $leading_text)) {
+          $this->raiseLintAtOffset(
+            $first->getOffset() - strlen($leading_text),
+            pht(
+              'Convention: no spaces before opening parenthesis in '.
+              'function and method declarations.'),
+            $leading_text,
+            '');
+        }
       }
 
       if (preg_match('/^\s+$/', $trailing_text)) {
@@ -50,6 +63,33 @@ final class ArcanistDeclarationParenthesesXHPASTLinterRule
             'function and method declarations.'),
           $trailing_text,
           '');
+      }
+
+      $use_list = $dec->getChildByIndex(4);
+      if ($use_list->getTypeName() == 'n_EMPTY') {
+        continue;
+      }
+      $use_token = $use_list->selectTokensOfType('T_USE');
+
+      foreach ($use_token as $use) {
+        $before = $use->getNonsemanticTokensBefore();
+        $after  = $use->getNonsemanticTokensAfter();
+
+        if (!$before) {
+          $this->raiseLintAtOffset(
+            $use->getOffset(),
+            pht('Convention: space before `%s` token.', 'use'),
+            '',
+            ' ');
+        }
+
+        if (!$after) {
+          $this->raiseLintAtOffset(
+            $use->getOffset() + strlen($use->getValue()),
+            pht('Convention: space after `%s` token.', 'use'),
+            '',
+            ' ');
+        }
       }
     }
   }
