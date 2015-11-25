8183a458048cc01e66d3a76f2e7da66f9e0e70fa
Joshua Spence <josh@freelancer.com>
2015-11-26 07:12:00 +1100

    Add a linter rule for `interface` method bodies
    Summary:
    `interface` methods cannot contain a body. This construct will cause a fatal error:
    
    ```
    PHP Fatal error:  Interface function X::Y() cannot contain body in /home/josh/workspace/github.com/phacility/arcanist/test.php on line 4
    ```
    
    Test Plan: Added unit tests.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D14561

---
+++ src/lint/linter/xhpast/rules/ArcanistInterfaceMethodBodyXHPASTLinterRule.php
@@ -0,0 +1,33 @@
+<?php
+
+final class ArcanistInterfaceMethodBodyXHPASTLinterRule
+  extends ArcanistXHPASTLinterRule {
+
+  const ID = 114;
+
+  public function getLintName() {
+    return pht('`%s` Method Cannot Contain Body', 'interface');
+  }
+
+  public function process(XHPASTNode $root) {
+    $interfaces = $root->selectDescendantsOfType('n_INTERFACE_DECLARATION');
+
+    foreach ($interfaces as $interface) {
+      $methods = $interface->selectDescendantsOfType('n_METHOD_DECLARATION');
+
+      foreach ($methods as $method) {
+        $body = $method->getChildByIndex(5);
+
+        if ($body->getTypeName() != 'n_EMPTY') {
+          $this->raiseLintAtNode(
+            $body,
+            pht(
+              '`%s` methods cannot contain a body. This construct will '.
+              'cause a fatal error.',
+              'interface'));
+        }
+      }
+    }
+  }
+
+}
