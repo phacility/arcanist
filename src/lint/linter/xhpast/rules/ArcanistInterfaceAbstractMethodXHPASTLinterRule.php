b323ad4d64848f0c4eb65390f6e6143c40c04136
Joshua Spence <josh@freelancer.com>
2015-11-26 07:21:29 +1100

    Add a linter rule for `abstract` methods within an `interface`
    Summary:
    `interface`s cannot contain `abstract` methods. This construct will cause a PHP fatal error:
    
    ```
    Access type for interface method SomeInterface::someMethod() must be omitted
    ```
    
    Test Plan: Added test cases.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D14562

---
+++ src/lint/linter/xhpast/rules/ArcanistInterfaceAbstractMethodXHPASTLinterRule.php
@@ -0,0 +1,34 @@
+<?php
+
+final class ArcanistInterfaceAbstractMethodXHPASTLinterRule
+  extends ArcanistXHPASTLinterRule {
+
+  const ID = 114;
+
+  public function getLintName() {
+    return pht('`%s` Methods Cannot Be Marked `%s`', 'interface', 'abstract');
+  }
+
+  public function process(XHPASTNode $root) {
+    $interfaces = $root->selectDescendantsOfType('n_INTERFACE_DECLARATION');
+
+    foreach ($interfaces as $interface) {
+      $methods = $interface->selectDescendantsOfType('n_METHOD_DECLARATION');
+
+      foreach ($methods as $method) {
+        $modifiers = $this->getModifiers($method);
+
+        if (idx($modifiers, 'abstract')) {
+          $this->raiseLintAtNode(
+            $method,
+            pht(
+              '`%s` methods cannot be marked as `%s`. This construct will '.
+              'cause a fatal error.',
+              'interface',
+              'abstract'));
+        }
+      }
+    }
+  }
+
+}
