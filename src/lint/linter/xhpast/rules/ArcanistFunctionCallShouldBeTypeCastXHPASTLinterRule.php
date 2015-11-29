578f540a92b1b78ca70b458244e39ea65f0c0ea8
Joshua Spence <josh@freelancer.com>
2015-11-30 07:32:43 +1100

    Add a linter rule for `*val` functions
    Summary: Type casts should be used instead of calls to the `*val` functions as function calls impose additional overhead.
    
    Test Plan: Added unit tests.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D14572

---
+++ src/lint/linter/xhpast/rules/ArcanistFunctionCallShouldBeTypeCastXHPASTLinterRule.php
@@ -0,0 +1,54 @@
+<?php
+
+final class ArcanistFunctionCallShouldBeTypeCastXHPASTLinterRule
+  extends ArcanistXHPASTLinterRule {
+
+  const ID = 105;
+
+  public function getLintName() {
+    return pht('Function Call Should Be Type Cast');
+  }
+
+  public function getLintSeverity() {
+    return ArcanistLintSeverity::SEVERITY_ADVICE;
+  }
+
+  public function process(XHPASTNode $root) {
+    static $cast_functions = array(
+      'boolval' => 'bool',
+      'doubleval' => 'double',
+      'floatval' => 'double',
+      'intval' => 'int',
+      'strval' => 'string',
+    );
+
+    $casts = $this->getFunctionCalls($root, array_keys($cast_functions));
+
+    foreach ($casts as $cast) {
+      $function_name = $cast
+        ->getChildOfType(0, 'n_SYMBOL_NAME')
+        ->getConcreteString();
+      $cast_name = $cast_functions[$function_name];
+
+      $parameters  = $cast->getChildOfType(1, 'n_CALL_PARAMETER_LIST');
+      $replacement = null;
+
+      // Only suggest a replacement if the function call has exactly
+      // one parameter.
+      if (count($parameters->getChildren()) == 1) {
+        $parameter   = $parameters->getChildByIndex(0);
+        $replacement = '('.$cast_name.')'.$parameter->getConcreteString();
+      }
+
+      $this->raiseLintAtNode(
+        $cast,
+        pht(
+          'For consistency, use `%s` (a type cast) instead of `%s` '.
+          '(a function call). Function calls impose additional overhead.',
+          '('.$cast_name.')',
+          $function_name),
+        $replacement);
+    }
+  }
+
+}
