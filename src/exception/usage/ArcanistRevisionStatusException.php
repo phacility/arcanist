5fcffc4f6945ce9b09e0f3ddc9bc81f96ad37e6b
Mira Szewczyk <miroslawa.szewczyk@tooploox.com>
2020-09-17 12:29:47 +0200

    Use Read More URL in exception

--- src/exception/usage/ArcanistRevisionStatusException.php
+++ src/exception/usage/ArcanistRevisionStatusException.php
@@ -5,8 +5,10 @@
  */
 final class ArcanistRevisionStatusException extends ArcanistUsageException {
 
-  public function __construct() {
-    parent::__construct(pht('Rejected: You should never land revision without review. If you know what you are doing and still want to land, use `FORCE_LAND=__reasson__` in revisions summary.'));
+  public function __construct($read_more_url) {
+    parent::__construct(
+      pht("Rejected: You should never land revision without review. If you know what you are doing and still want to land, use `FORCE_LAND=__reasson__` in revisions summary. Read more: {$read_more_url}")
+    );
   }
 
 }
