8c1c82a75ffd6ad0ecb45d1f3a2f99e22fa9b930
mattlwilliams <mattlwilliams@gmail.com>
2021-06-25 11:12:03 -0700

    Try to fetch base ref explicitly before fetch all (#230)

--- src/workflow/ArcanistPatchWorkflow.php
+++ src/workflow/ArcanistPatchWorkflow.php
@@ -511,17 +511,18 @@ EOTEXT
           "<bg:blue>** %s **</bg> %s\n",
           pht('INFO'),
           pht('Base commit is not in local repository; trying to fetch.'));
-        $repository_api->execManualLocal('fetch --quiet --all');
+        // UBER CODE
+        $this->authenticateConduit();
+        $this->pullFromAllRemotesUntilFound($bundle->getBaseRevision());
         $has_base_revision = $repository_api->hasLocalCommit(
           $bundle->getBaseRevision());
-        // UBER CODE
         if (!$has_base_revision) {
-          $this->authenticateConduit();
-          $this->pullFromAllRemotesUntilFound($bundle->getBaseRevision());
+          // UBER CODE
+          $repository_api->execManualLocal('fetch --quiet --all');
           $has_base_revision = $repository_api->hasLocalCommit(
             $bundle->getBaseRevision());
         }
-        // UBER CODE
+
       }
     }
 
