06b908e9665474b348a0572fb82c159819c2dbad
Tianxiang Chen <tianxiang.chen@robinhood.com>
2021-08-18 14:29:30 -0700

    Introduce auto land in arc diff create template

--- src/workflow/ArcanistDiffWorkflow.php
+++ src/workflow/ArcanistDiffWorkflow.php
@@ -1449,6 +1449,11 @@ EOTEXT
 
     $issues = array_merge(
       array(
+        pht('*NEW FEATURE*: auto land (http://go/autolandwiki)'),
+        pht('To use auto land, add `Tags: #ready_to_land` in this commit message'),
+        '',
+      ),
+      array(
         pht('NEW DIFFERENTIAL REVISION'),
         pht('Describe the changes in this new revision.'),
       ),
