789d5d43ef75e6c5bfb8267469208e2408abfd18
Jakub Brož <brozikcz@gmail.com>
2023-02-20 09:56:58 +0100

    Fix strlen issue with null in binary changes

--- src/parser/ArcanistBundle.php
+++ src/parser/ArcanistBundle.php
@@ -762,7 +762,7 @@ final class ArcanistBundle extends Phobject {
       $old_data = $this->getBlob($old_phid, $name);
     }
 
-    $old_length = strlen($old_data);
+    $old_length = strlen($old_data ?? '');
 
     // Here, and below, the binary will be emitted with base85 encoding. This
     // encoding encodes each 4 bytes of input in 5 bytes of output, so we may
