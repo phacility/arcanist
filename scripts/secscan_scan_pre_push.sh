d9c5e0be6555c2d5916b63032f968c7e4bddbe98
Yi <13973239+yi-lu@users.noreply.github.com>
2023-10-24 03:45:11 -0700

    minor improvement to secscan pre-push script (#32)

--- scripts/secscan_scan_pre_push.sh
+++ scripts/secscan_scan_pre_push.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
-set +o errexit
-REPO_PATH="$(git rev-parse --show-toplevel)"
-BASE_COMMIT=$(
-  git log \
-    --oneline \
-    --committer="phlq" \
-    --pretty="format:%H" \
-    --max-count=50 \
-    | head -1
-)
+set -euo pipefail
 
-if [ $ENABLE_SECRET_DETECTION_PROCESS = "true" ]; then
-  bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //secscan/tools/trufflehog:scan_local -- $REPO_PATH $BASE_COMMIT
+set +e
+
+REPO_PATH="$(git rev-parse --show-toplevel)"
+
+if [ ${ENABLE_SECRET_DETECTION_PROCESS:-} = "true" ]; then
+  BASE_COMMIT=$(
+    git log \
+      --oneline \
+      --committer="phlq" \
+      --pretty="format:%H" \
+      --max-count=50 \
+      | head -1
+  )
+
+  bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //secscan/tools/trufflehog:scan_local -- "$REPO_PATH" "$BASE_COMMIT"
 else
   bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //secscan -- scan -d "${REPO_PATH}" -s=pre-commit --timeout=8 --report-metrics=true --log-file="/tmp/secscan.log"
 fi
+
 EXIT_CODE=$?
 
-set -o errexit
+set -e
 
 # Secscan exits with code 4 only in case where it has findings,
 # we should only block the diff in that scenario.
